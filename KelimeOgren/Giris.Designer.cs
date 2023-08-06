60bc8681f91d271160700a020bfe5c60388b3bcf
Vural Sayan <vuralsayan01@gmail.com>
2023-08-06 15:00:28 +0300

    Kullanıcı giriş ekranı oluşturuldu

---
+++ KelimeOgren/Giris.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace KelimeOgren
+{
+    partial class Giris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtKullanıcıAd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 125);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı:";
+            // 
+            // TxtKullanıcıAd
+            // 
+            this.TxtKullanıcıAd.Location = new System.Drawing.Point(280, 118);
+            this.TxtKullanıcıAd.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.TxtKullanıcıAd.Name = "TxtKullanıcıAd";
+            this.TxtKullanıcıAd.Size = new System.Drawing.Size(172, 34);
+            this.TxtKullanıcıAd.TabIndex = 1;
+            // 
+            // Giris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 280);
+            this.Controls.Add(this.TxtKullanıcıAd);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Giris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Giris";
+            this.Load += new System.EventHandler(this.Giris_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtKullanıcıAd;
+    }
+}
---
+++ KelimeOgren/Giris.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KelimeOgren
+{
+    public partial class Giris : Form
+    {
+        public Giris()
+        {
+            InitializeComponent();
+        }
+
+        private void Giris_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
